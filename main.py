66820242c79c165691442dd72b8213a215f709e3
Hubble06 <89403240+Hubble06@users.noreply.github.com>
2022-08-24 09:15:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('니얼굴')
+    print_hi('안녕?')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
